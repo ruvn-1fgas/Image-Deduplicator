16207c655586a8796ca6eec189e1889a76511298
Рустам Расимович <horrorvanchugov@gmail.com>
2023-03-18 01:43:20 +0300

    Update settings.cpp

--- structures/settings.cpp
+++ structures/settings.cpp
@@ -13,59 +13,38 @@ namespace settings
 
     void loadSettings()
     {
-        // check if file exists
         if (!std::filesystem::exists("settings.ini"))
         {
-            // create file
             std::ofstream file("settings.ini");
             file << "[settings]\nRecursive = false\nHash treshold = 80\nApp theme = default\n";
             file.close();
         }
 
-        // open ini file
         std::ifstream file("settings.ini");
         std::string line;
 
-        /*
-        hash structure is :
-        [settings]
-        Recursive = false		; Determines whether the program will recursively search for images
-        Hash treshold = 80		; Defines a treshold for hash comparing
-        App theme = default
-        */
-
-        // read file line by line
         while (std::getline(file, line))
         {
-            // check if line is a comment
             if (line[0] == ';')
                 continue;
 
-            // check if line is a setting
             if (line.find("Recursive") != std::string::npos)
             {
-                // get value
                 std::string value = line.substr(line.find('=') + 2);
-                // convert to bool
                 recursive = value == "true" ? true : false;
             }
             else if (line.find("Hash treshold") != std::string::npos)
             {
-                // get value
                 std::string value = line.substr(line.find('=') + 2);
-                // convert to int
                 hashTreshold = std::stoi(value);
             }
             else if (line.find("App theme") != std::string::npos)
             {
-                // get value
                 std::string value = line.substr(line.find('=') + 2);
-                // convert to int
                 appTheme = value == "default" ? 0 : 1;
             }
         }
 
-        // print result
         std::cout << "Recursive: " << recursive << std::endl;
         std::cout << "Hash treshold: " << hashTreshold << std::endl;
         std::cout << "App theme: " << appTheme << std::endl;
