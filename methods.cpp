a5069788ea5b0fae6e97c86261d071b7fc48057c
Рустам Расимович <horrorvanchugov@gmail.com>
2023-03-25 18:04:31 +0300

    = fix threads

--- methods.cpp
+++ methods.cpp
@@ -3,6 +3,7 @@
 #include "imageLib/image.cpp"
 #include "structures/settings.cpp"
 #include <thread>
+#include <mutex>
 #include <atomic>
 #include <chrono>
 
@@ -69,12 +70,12 @@ std::vector<std::vector<std::wstring>> compareImages(std::wstring directoryPath,
 
     std::vector<std::vector<std::wstring>> listOfDuplicates;
 
-    for (int i = 0; i < duplicates.size(); i++)
+    for (size_t i = 0; i < duplicates.size(); i++)
     {
         std::vector<std::wstring> duplicatesList;
         duplicatesList.push_back(duplicates[i].first);
         duplicatesList.push_back(duplicates[i].second);
-        for (int j = i + 1; j < duplicates.size(); j++)
+        for (size_t j = i + 1; j < duplicates.size(); j++)
         {
             if (duplicates[i].first == duplicates[j].first)
             {
@@ -117,8 +118,8 @@ bool isThreaded = false;
 bool compareHash(std::vector<bool> hash1, std::vector<bool> hash2)
 {
 
-    int count = 0;
-    for (int i = 0; i < hash1.size(); i++)
+    size_t count = 0;
+    for (size_t i = 0; i < hash1.size(); i++)
         if (hash1[i] == hash2[i])
             count++;
 
@@ -127,7 +128,6 @@ bool compareHash(std::vector<bool> hash1, std::vector<bool> hash2)
 
 std::vector<pair> phashMethod(std::vector<std::wstring> images, GtkWidget *progressBar)
 {
-    std::vector<pair> duplicates;
     std::vector<bool> visited(images.size(), false);
     std::vector<std::vector<bool>> hashes(images.size());
 
@@ -136,47 +136,37 @@ std::vector<pair> phashMethod(std::vector<std::wstring> images, GtkWidget *progr
     if (isThreaded)
     {
         std::vector<std::thread> threads;
-        std::atomic<int> i = 0;
-        for (int j = 0; j < std::thread::hardware_concurrency(); j++)
+        std::atomic<size_t> i = 0;
+
+        for (size_t j = 0; j < std::thread::hardware_concurrency(); j++)
         {
             threads.push_back(std::thread([&]()
                                           {
                 while (i < images.size())
                 {
-                    int index = i++;
-                    if (index >= images.size() || i >= images.size())
-                        break;
+                    size_t index = i++;
+
+                    if(index >= images.size())
+                        return;
 
                     Image img = getImage(images[index]);
                     hashes[index] = img.pHash();
                 } }));
+            threads[j].detach();
         }
 
         while (i < images.size())
         {
-            for (auto &t : threads)
-                if (t.joinable())
-                    t.join();
-
-            if (i >= images.size())
-                break;
-
             gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progressBar), i / (double)images.size());
-
             std::string hashCalc = language::dict["StartButton.HashCalcAction"][settings::language];
             char *text = g_strdup_printf((hashCalc + "%d/%d").c_str(), i + 1, images.size());
             gtk_progress_bar_set_text(GTK_PROGRESS_BAR(progressBar), text);
             while (g_main_context_pending(NULL))
                 g_main_context_iteration(NULL, FALSE);
         }
-
-        for (auto &t : threads)
-            if (t.joinable())
-                t.join();
-        threads.clear();
     }
     else
-        for (int i = 0; i < images.size(); i++)
+        for (size_t i = 0; i < images.size(); i++)
         {
             Image img = getImage(images[i]);
             hashes[i] = img.pHash();
@@ -192,58 +182,62 @@ std::vector<pair> phashMethod(std::vector<std::wstring> images, GtkWidget *progr
     auto end = std::chrono::high_resolution_clock::now();
 
     if (isThreaded)
-    {
-        std::cout << "Hash Calc - Threaded version: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
-    }
+        std::cout << "Hash Calc - Threaded version: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
     else
-    {
-        std::cout << "Hash Calc - Non-threaded version: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
-    }
+        std::cout << "Hash Calc - Non-threaded version: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
 
     start = std::chrono::high_resolution_clock::now();
 
+    std::vector<pair> duplicates;
+
     if (isThreaded)
     {
+        std::mutex guard;
+
         std::vector<std::thread> threads;
-        std::atomic<int> i = 0;
-        for (int j = 0; j < std::thread::hardware_concurrency(); j++)
+        std::atomic<size_t> i = 0;
+        for (size_t j = 0; j < std::thread::hardware_concurrency() - 1; j++)
         {
-            threads.push_back(std::thread([&]()
-                                          {
+            threads.emplace_back([&]()
+                                 {
                 while (i < hashes.size())
                 {
-                    int index = i++;
-                    if(index >= hashes.size() || i >= hashes.size())
-                        break;
-                    
+                    size_t index = i++;
+
+                    if (index >= images.size())
+                        return;
+
                     if (visited[index])
                         continue;
-                    for (int j = index + 1; j < hashes.size(); j++)
+
+                    for (size_t j = index + 1; j < hashes.size(); j++)
                     {
+                        if (j >= images.size())
+                            return;
+
                         if (visited[j])
                             continue;
+
                         if (compareHash(hashes[index], hashes[j]))
                         {
                             pair p;
                             p.first = images[index];
                             p.second = images[j];
+                            guard.lock();
+                            
                             duplicates.push_back(p);
-
+                            
+                            guard.unlock();
                             visited[j] = true;
                         }
                     }
-                } }));
+                } });
+
+            threads[j].detach();
         }
 
         while (i < hashes.size())
         {
-            for (auto &t : threads)
-                if (t.joinable())
-                    t.join();
-
-            if (i >= hashes.size())
-                break;
-
             gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progressBar), i / (double)hashes.size());
             std::string imgComp = language::dict["StartButton.ImageCompareAction"][settings::language];
             char *text = g_strdup_printf((imgComp + "%d%%").c_str(), (int)(i / (double)hashes.size() * 100));
@@ -252,18 +246,14 @@ std::vector<pair> phashMethod(std::vector<std::wstring> images, GtkWidget *progr
                 g_main_context_iteration(NULL, FALSE);
         }
 
-        for (auto &t : threads)
-            if (t.joinable())
-                t.join();
-
         threads.clear();
     }
     else
-        for (int i = 0; i < hashes.size(); i++)
+        for (size_t i = 0; i < hashes.size(); i++)
         {
             if (visited[i])
                 continue;
-            for (int j = i + 1; j < hashes.size(); j++)
+            for (size_t j = i + 1; j < hashes.size(); j++)
             {
                 if (visited[j])
                     continue;
@@ -282,7 +272,7 @@ std::vector<pair> phashMethod(std::vector<std::wstring> images, GtkWidget *progr
 
             gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progressBar), i / (double)hashes.size());
             std::string imgComp = language::dict["StartButton.ImageCompareAction"][settings::language];
-            char *text = g_strdup_printf((imgComp + "%d%%").c_str(), (int)(i / (double)hashes.size() * 100));
+            char *text = g_strdup_printf((imgComp + "%d%%").c_str(), (size_t)(i / (double)hashes.size() * 100));
             gtk_progress_bar_set_text(GTK_PROGRESS_BAR(progressBar), text);
             while (g_main_context_pending(NULL))
                 g_main_context_iteration(NULL, FALSE);
@@ -291,13 +281,9 @@ std::vector<pair> phashMethod(std::vector<std::wstring> images, GtkWidget *progr
     end = std::chrono::high_resolution_clock::now();
 
     if (isThreaded)
-    {
-        std::cout << "Image Compare - Threaded version: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
-    }
+        std::cout << "Image Compare - Threaded version: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
     else
-    {
-        std::cout << "Image Compare - Non-threaded version: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
-    }
+        std::cout << "Image Compare - Non-threaded version: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
 
     for (auto hash : hashes)
         hash.clear();
