d11d4d0aa4c86377e2cd05d4c38b08bcbf9e51b5
Рустам Расимович <horrorvanchugov@gmail.com>
2023-05-22 22:06:15 +0300

    = nah

--- include/gui/dupl_window.hpp
+++ include/gui/dupl_window.hpp
@@ -33,118 +33,49 @@ private:
     void UpdateListBoxItems();
 
     // Callbacks
+public:
+    void OnPrevButtonClicked(GtkWidget *button);
+    void OnNextButtonClicked(GtkWidget *button);
+    void OnListBoxRowActivated(GtkListBox *box, GtkListBoxRow *row);
+    void OnListBoxRightClickPressed(GtkGesture *gesture, gint n_press, gdouble x, gdouble y);
+    void OnListBoxLabelQueryTooltip(GtkWidget *widget, gint x, gint y, gboolean keyboard_mode, GtkTooltip *tooltip);
+    void OpenAllFiles(GSimpleAction *action, GVariant *parameter);
+    void DeleteAllFiles(GSimpleAction *action, GVariant *parameter);
+
 private:
-    // done
-    virtual void OnPrevButtonClicked(GtkWidget *button, gpointer data);
-    // done
-    virtual void OnNextButtonClicked(GtkWidget *button, gpointer data);
-    // done
-    virtual void OnListBoxRowActivated(GtkListBox *box, GtkListBoxRow *row, gpointer data);
-    // done
-    virtual void OnListBoxRightClickPressed(GtkGesture *gesture, gint n_press, gdouble x, gdouble y, gpointer data);
-    // done
-    virtual void OnListBoxLabelQueryTooltip(GtkWidget *widget, gint x, gint y, gboolean keyboard_mode, GtkTooltip *tooltip, gpointer data);
-    // done
-    virtual void OpenAllFiles(GSimpleAction *action, GVariant *parameter, gpointer data);
-    // done
-    virtual void DeleteAllFiles(GSimpleAction *action, GVariant *parameter, gpointer data);
-
-    void PrevButtonClicked(GtkWidget *button, gpointer data)
-    {
-        g_signal_connect(
-            button,
-            "clicked",
-            G_CALLBACK(PrevButtonClickedCallback), this);
-    }
-
-    void NextButtonClicked(GtkWidget *button, gpointer data)
-    {
-        g_signal_connect(
-            button,
-            "clicked",
-            G_CALLBACK(NextButtonClickedCallback), this);
-    }
-
-    void ListBoxRowActivated(GtkListBox *box, GtkListBoxRow *row, gpointer data)
-    {
-        g_signal_connect(
-            box,
-            "row-selected",
-            G_CALLBACK(ListBoxRowActivatedCallback), this);
-    }
-
-    void ListBoxRightClickPressed(GtkGesture *gesture, gint n_press, gdouble x, gdouble y, gpointer data)
-    {
-        g_signal_connect(
-            gesture,
-            "pressed",
-            G_CALLBACK(ListBoxRightClickPressedCallback), this);
-    }
-
-    void ListBoxLabelQueryTooltip(GtkWidget *label, gint x, gint y, gboolean keyboard_mode, GtkTooltip *tooltip, gpointer data)
-    {
-        g_signal_connect(
-            label,
-            "query-tooltip",
-            G_CALLBACK(ListBoxLabelQueryTooltipCallback), this);
-    }
-
-    void OpenAllFilesClicked(GSimpleAction *action, GVariant *parameter, gpointer data)
-    {
-        g_signal_connect(
-            action,
-            "activate",
-            G_CALLBACK(OpenAllFilesCallback), this);
-    }
-
-    void DeleteAllFilesClicked(GSimpleAction *action, GVariant *parameter, gpointer data)
-    {
-        g_signal_connect(
-            action,
-            "activate",
-            G_CALLBACK(DeleteAllFilesCallback), this);
-    }
-
     static void PrevButtonClickedCallback(GtkWidget *button, gpointer data)
     {
-        DuplWindow *window = static_cast<DuplWindow *>(data);
-        window->OnPrevButtonClicked(button, data);
+        reinterpret_cast<DuplWindow *>(data)->OnPrevButtonClicked(button);
     }
 
     static void NextButtonClickedCallback(GtkWidget *button, gpointer data)
     {
-        DuplWindow *window = static_cast<DuplWindow *>(data);
-        window->OnNextButtonClicked(button, data);
+        reinterpret_cast<DuplWindow *>(data)->OnNextButtonClicked(button);
     }
 
     static void ListBoxRowActivatedCallback(GtkListBox *box, GtkListBoxRow *row, gpointer data)
     {
-        DuplWindow *window = static_cast<DuplWindow *>(data);
-        window->OnListBoxRowActivated(box, row, data);
+        reinterpret_cast<DuplWindow *>(data)->OnListBoxRowActivated(box, row);
     }
 
     static void ListBoxRightClickPressedCallback(GtkGesture *gesture, gint n_press, gdouble x, gdouble y, gpointer data)
     {
-        DuplWindow *window = static_cast<DuplWindow *>(data);
-        window->OnListBoxRightClickPressed(gesture, n_press, x, y, data);
+        reinterpret_cast<DuplWindow *>(data)->OnListBoxRightClickPressed(gesture, n_press, x, y);
     }
 
     static void ListBoxLabelQueryTooltipCallback(GtkWidget *label, gint x, gint y, gboolean keyboard_mode, GtkTooltip *tooltip, gpointer data)
     {
-        DuplWindow *window = static_cast<DuplWindow *>(data);
-        window->OnListBoxLabelQueryTooltip(label, x, y, keyboard_mode, tooltip, data);
+        reinterpret_cast<DuplWindow *>(data)->OnListBoxLabelQueryTooltip(label, x, y, keyboard_mode, tooltip);
     }
 
     static void OpenAllFilesCallback(GSimpleAction *action, GVariant *parameter, gpointer data)
     {
-        DuplWindow *window = static_cast<DuplWindow *>(data);
-        window->OpenAllFiles(action, parameter, data);
+        reinterpret_cast<DuplWindow *>(data)->OpenAllFiles(action, parameter);
     }
 
     static void DeleteAllFilesCallback(GSimpleAction *action, GVariant *parameter, gpointer data)
     {
-        DuplWindow *window = static_cast<DuplWindow *>(data);
-        window->DeleteAllFiles(action, parameter, data);
+        reinterpret_cast<DuplWindow *>(data)->DeleteAllFiles(action, parameter);
     }
 };
 
