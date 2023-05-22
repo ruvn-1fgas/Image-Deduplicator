75582e4b90b83fdff60afd941ffc366f21ec7a43
Рустам Расимович <horrorvanchugov@gmail.com>
2023-05-22 13:34:36 +0300

    - remove icon

--- src/gui/main_window.cpp
+++ src/gui/main_window.cpp
@@ -30,11 +30,6 @@ void MainWindow::SetupWindow()
     gtk_window_set_application(GTK_WINDOW(this->window_), this->app_);
     gtk_window_set_title(GTK_WINDOW(this->window_), "Image Deduplicator");
 
-    // Set the window icon
-    auto icon_pixbuf = gdk_pixbuf_new_from_inline(-1, img_dedup_icon, FALSE, NULL);
-    auto icon_texture = gdk_texture_new_for_pixbuf(icon_pixbuf);
-    gtk_window_set_icon(GTK_WINDOW(this->window_), icon_texture);
-
     gtk_window_set_resizable(GTK_WINDOW(this->window_), FALSE);
 
     // MAIN GRID SETUP
